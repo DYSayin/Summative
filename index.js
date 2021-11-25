4eae7a8f11e49a611c57a9b3dfaf3f979248b665
DYSayin <ysfsyn.3180@gmail.com>
2021-11-25 19:56:38 +0000

    push

---
+++ index.js
@@ -0,0 +1,11 @@
+function myFunction() {
+    alert("Invalid input,Please try again");
+  }
+
+  function getInputValue(){
+    // Selecting the input element and get its value 
+    var inputVal = document.getElementById("myInput").value;
+    
+    // Displaying the value
+    console.log(inputVal);
+}
