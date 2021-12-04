7e78aa82a9c44fb863c3dba2094a7749fad64fda
DYSayin <ysfsyn.3180@gmail.com>
2021-12-04 11:08:07 +0000

    base project uploaded

--- index.js
+++ index.js
@@ -1,11 +1,59 @@
 function myFunction() {
     alert("Invalid input,Please try again");
-  }
+}
 
-  function getInputValue(){
-    // Selecting the input element and get its value 
-    var inputVal = document.getElementById("myInput").value;
-    
-    // Displaying the value
-    console.log(inputVal);
+function getInputValue(){
+  var inputVal = document.getElementById("myInput").value;
+  console.log(inputVal)  
+}
+
+
+
+/* Table's colour buttons */
+function changecolour1() {
+  var x = document.querySelectorAll("table");
+  var i;
+  for (i = 0; i < x.length; i++) {
+      x[i].style.backgroundColor = "aqua";
+  }
+}
+
+function changecolour2() {
+  var x = document.querySelectorAll("table");
+  var i;
+  for (i = 0; i < x.length; i++) {
+      x[i].style.backgroundColor = "gray";
+  }
+}
+
+function changecolour3() {
+  var x = document.querySelectorAll("table");
+  var i;
+  for (i = 0; i < x.length; i++) {
+      x[i].style.backgroundColor = "green";
+  }
+}
+
+function changecolour4() {
+  var x = document.querySelectorAll("table");
+  var i;
+  for (i = 0; i < x.length; i++) {
+      x[i].style.backgroundColor = "red";
+  }
+}
+
+function changecolour5() {
+  var x = document.querySelectorAll("table");
+  var i;
+  for (i = 0; i < x.length; i++) {
+      x[i].style.backgroundColor = "orange";
+  }
+}
+
+function changecolour6() {
+  var x = document.querySelectorAll("table");
+  var i;
+  for (i = 0; i < x.length; i++) {
+      x[i].style.backgroundColor = "white";
+  }
 }
