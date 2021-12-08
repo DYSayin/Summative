8cc8bd6d95db5237b1c9656fff9a0efe3703efbd
DYSayin <ysfsyn.3180@gmail.com>
2021-12-08 10:29:55 +0000

    jsdoc documenting

--- index.js
+++ index.js
@@ -1,17 +1,49 @@
 /* eslint-disable no-unused-vars */
+
+/**
+ * Create a greeting message at the top of the console
+ * @date 2021-12-08
+ * @param {any} greeting 
+ * @returns "Welcome To My Webpage!"
+ */
+function greetings() {
+  var greeting = "Welcome To My Webpage!";
+  console.log(greeting);
+}
+
+greetings();
+
 // eslint-disable-next-line no-unused-vars
+/**
+ * Creates an alert when user click the search button.
+ * @date 2021-12-08
+ * @param {any} 
+ * @returns the following string: "Invalid input, Please try again"
+ */
 function myFunction() {
-    alert("Invalid input,Please try again");
+    alert("Invalid input, Please try again");
 }
 
+
+/**
+ * Creates an alert when user submit the form.
+ * @date 2021-12-08
+ * @param {any} 
+ * @returns the following string: "Thank you for submitting. We will keep in touch with you!"
+ */
 function myFunction2() {
-  alert("Thank you for submittig. We will keep in touch with you!");
+  alert("Thank you for submitting. We will keep in touch with you!");
 }
-
 
 
 /* Table's colour buttons */
 // eslint-disable-next-line no-unused-vars
+/**
+ * Change the whole table's colour when the user click the button
+ * @date 2021-12-08
+ * @param {any} "table"
+ * @returns "aqua" background colour
+ */
 function changecolour1() {
   var x = document.querySelectorAll("table");
   var i;
@@ -20,6 +52,13 @@ function changecolour1() {
   }
 }
 
+
+/**
+ * Change the whole table's colour when the user click the button
+ * @date 2021-12-08
+ * @param {any} "table"
+ * @returns "orange" background colour
+ */
 function changecolour2() {
   var x = document.querySelectorAll("table");
   var i;
@@ -28,6 +67,12 @@ function changecolour2() {
   }
 }
 
+/**
+ * Change the whole table's colour when the user click the button
+ * @date 2021-12-08
+ * @param {any} "table"
+ * @returns "green" background colour
+ */
 function changecolour3() {
   var x = document.querySelectorAll("table");
   var i;
@@ -36,6 +81,12 @@ function changecolour3() {
   }
 }
 
+/**
+ * Change the whole table's colour when the user click the button
+ * @date 2021-12-08
+ * @param {any} "table"
+ * @returns "red" background colour
+ */
 function changecolour4() {
   var x = document.querySelectorAll("table");
   var i;
@@ -46,6 +97,12 @@ function changecolour4() {
 
 
 
+/**
+ *Change the whole table's colour when the user click the button
+ * @date 2021-12-08
+ * @param {any} "table"
+ * @returns "white" background colour
+ */
 function changecolour5() {
   var x = document.querySelectorAll("table");
   var i;
