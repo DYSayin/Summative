2630950cb10aae30af2bf0f8b626ee5907aa1cdd
DYSayin <ysfsyn.3180@gmail.com>
2021-12-10 11:48:45 +0000

    changes

--- index.js
+++ index.js
@@ -3,11 +3,11 @@
 /**
  * Create a greeting message at the top of the console
  * @date 2021-12-08
- * @param {any} greeting 
+ * @param {string} greeting
  * @returns "Welcome To My Webpage!"
  */
-function greetings() {
-  var greeting = "Welcome To My Webpage!";
+function greetings () {
+  const greeting = 'Welcome To My Webpage!';
   console.log(greeting);
 }
 
@@ -17,24 +17,22 @@ greetings();
 /**
  * Creates an alert when user click the search button.
  * @date 2021-12-08
- * @param {any} 
+ * @param {any}
  * @returns the following string: "Invalid input, Please try again"
  */
-function myFunction() {
-    alert("Invalid input, Please try again");
+function myFunction () {
+    alert('Invalid input, Please try again');
 }
-
 
 /**
  * Creates an alert when user submit the form.
  * @date 2021-12-08
- * @param {any} 
+ * @param {any}
  * @returns the following string: "Thank you for submitting. We will keep in touch with you!"
  */
-function myFunction2() {
-  alert("Thank you for submitting. We will keep in touch with you!");
+function myFunction2 () {
+  alert('Thank you for submitting. We will keep in touch with you!');
 }
-
 
 /* Table's colour buttons */
 // eslint-disable-next-line no-unused-vars
@@ -44,14 +42,13 @@ function myFunction2() {
  * @param {any} "table"
  * @returns "aqua" background colour
  */
-function changecolour1() {
-  var x = document.querySelectorAll("table");
-  var i;
+function changecolour1 () {
+  const x = document.querySelectorAll('table');
+  let i;
   for (i = 0; i < x.length; i++) {
-      x[i].style.backgroundColor = "aqua";
+      x[i].style.backgroundColor = 'aqua';
   }
 }
-
 
 /**
  * Change the whole table's colour when the user click the button
@@ -59,11 +56,11 @@ function changecolour1() {
  * @param {any} "table"
  * @returns "orange" background colour
  */
-function changecolour2() {
-  var x = document.querySelectorAll("table");
-  var i;
+function changecolour2 () {
+  const x = document.querySelectorAll('table');
+  let i;
   for (i = 0; i < x.length; i++) {
-      x[i].style.backgroundColor = "orange";
+      x[i].style.backgroundColor = 'orange';
   }
 }
 
@@ -73,11 +70,11 @@ function changecolour2() {
  * @param {any} "table"
  * @returns "green" background colour
  */
-function changecolour3() {
-  var x = document.querySelectorAll("table");
-  var i;
+function changecolour3 () {
+  const x = document.querySelectorAll('table');
+  let i;
   for (i = 0; i < x.length; i++) {
-      x[i].style.backgroundColor = "green";
+      x[i].style.backgroundColor = 'green';
   }
 }
 
@@ -87,15 +84,13 @@ function changecolour3() {
  * @param {any} "table"
  * @returns "red" background colour
  */
-function changecolour4() {
-  var x = document.querySelectorAll("table");
-  var i;
+function changecolour4 () {
+  const x = document.querySelectorAll('table');
+  let i;
   for (i = 0; i < x.length; i++) {
-      x[i].style.backgroundColor = "red";
+      x[i].style.backgroundColor = 'red';
   }
 }
-
-
 
 /**
  *Change the whole table's colour when the user click the button
@@ -103,12 +98,10 @@ function changecolour4() {
  * @param {any} "table"
  * @returns "white" background colour
  */
-function changecolour5() {
-  var x = document.querySelectorAll("table");
-  var i;
+function changecolour5 () {
+  const x = document.querySelectorAll('table');
+  let i;
   for (i = 0; i < x.length; i++) {
-      x[i].style.backgroundColor = "white";
+      x[i].style.backgroundColor = 'white';
   }
 }
-
-
